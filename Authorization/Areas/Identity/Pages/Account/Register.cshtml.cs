fff997f6f52c41f29b11a0b919bd5c5ce5370430
Haruo-no-Hokusai <bloodkingdom242547@gmail.com>
2023-12-23 15:55:28 +0700

    Low DropDown

--- Authorization/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Authorization/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -19,6 +19,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Authorization.Areas.Identity.Pages.Account
 {
@@ -30,13 +32,15 @@ namespace Authorization.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<AuthorizationUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
         public RegisterModel(
             UserManager<AuthorizationUser> userManager,
             IUserStore<AuthorizationUser> userStore,
             SignInManager<AuthorizationUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _userStore = userStore;
@@ -44,6 +48,7 @@ namespace Authorization.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _roleManager = roleManager;
         }
 
         /// <summary>
@@ -98,11 +103,26 @@ namespace Authorization.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            public string Role {  get; set; }
+            [ValidateNever]
+            public IEnumerable<SelectListItem> RoleList { get; set; }
         }
 
 
         public async Task OnGetAsync(string returnUrl = null)
         {
+            if (User.Identity.IsAuthenticated) Response.Redirect("/");
+            Input = new InputModel
+            {
+                RoleList = _roleManager.Roles.Select(r => r.Name).Select(name => new SelectListItem
+                {
+                    Text = name,
+                    Value = name
+                })
+            };
+
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
